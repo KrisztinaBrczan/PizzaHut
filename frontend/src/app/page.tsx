a72beaef2909372af13c87b384a878d88de5971d
KrisztinaBrczan <brczan.kriszti@gmail.com>
2025-03-24 21:30:53 +0100

    format pizzaCard

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -3,36 +3,48 @@
 import PizzaCard from '@/components/PizzaCard';
 import getAllPizzas from '@/services/getAllPizzas';
 import { Pizzas } from '@/Types';
-import { Box, Card, Grid2, Typography } from '@mui/material';
+import { Box, CircularProgress, Grid2, Typography } from '@mui/material';
 import { useQuery } from '@tanstack/react-query';
 
 export default function Home() {
-  const { data: pizzas } = useQuery<Pizzas[]>({
+  const { data: pizzas, isLoading } = useQuery<Pizzas[]>({
     queryFn: async () => await getAllPizzas(),
 
     queryKey: ['pizzas'],
   });
 
+  if (isLoading) {
+    return (
+      <Box display="flex" justifyContent="center" alignItems="center" height="50vh">
+        <CircularProgress />
+      </Box>
+    );
+  }
+
   return (
     <>
-      <Typography variant="h5" align="center" sx={{ fontWeight: 'bold' }}>
-        Our Pizzas
-      </Typography>
-      <Grid2
-        container
-        spacing={4}
-        justifyContent="center"
-        sx={{
-          flexDirection: { xs: 'column', sm: 'row' },
-          marginTop: '35px',
-        }}
-      >
-        {pizzas?.map((currentPizza) => (
-          <Grid2 key={currentPizza.id} container justifyContent="center">
-            <PizzaCard currentPizza={currentPizza} />
+      {pizzas && (
+        <Box>
+          <Typography variant="h5" align="center" sx={{ fontWeight: 'bold' }}>
+            Our Pizzas
+          </Typography>
+          <Grid2
+            container
+            spacing={4}
+            justifyContent="center"
+            sx={{
+              flexDirection: { xs: 'column', sm: 'row' },
+              marginTop: '35px',
+            }}
+          >
+            {pizzas.map((currentPizza) => (
+              <Grid2 key={currentPizza.id} container justifyContent="center">
+                <PizzaCard currentPizza={currentPizza} />
+              </Grid2>
+            ))}
           </Grid2>
-        ))}
-      </Grid2>
+        </Box>
+      )}
     </>
   );
 }
--- frontend/src/components/PizzaCard.tsx
+++ frontend/src/components/PizzaCard.tsx
@@ -1,21 +1,52 @@
+'use client';
+
 import { Pizzas } from '@/Types';
-import { Button, Card, CardActions, CardContent, CardMedia, Typography } from '@mui/material';
+import { Box, Button, Card, CardActions, CardContent, CardMedia, Grid2, Typography } from '@mui/material';
 
 interface PizzaCardProps {
   currentPizza: Pizzas;
 }
 
 export default function PizzaCard({ currentPizza }: PizzaCardProps) {
+  console.log('currentpizza', currentPizza);
+
   return (
     <Card sx={{ width: '250px' }}>
       <CardMedia component="img" sx={{ height: 140, objectFit: 'fit' }} image={currentPizza.photo_url} title={currentPizza.name} />
-      <CardContent>
+      <CardContent sx={{ height: '200px' }}>
         <Typography align="center" sx={{ fontWeight: 'bold' }}>
           {currentPizza.price} HUF
         </Typography>
-        <Typography align="center" variant="h6">
+        <Typography align="center" variant="h6" sx={{ marginTop: '8px' }}>
           {currentPizza.name}
         </Typography>
+
+        <Grid2 container spacing={2} justifyContent="center" sx={{ marginTop: '16px' }}>
+          {currentPizza.diet.map((diet) => (
+            <Typography
+              key={diet}
+              sx={{
+                border: '0.5px solid',
+                borderColor: 'secondary.main',
+                borderRadius: '25px',
+                textAlign: 'center',
+                padding: '5px',
+                fontSize: '12px',
+              }}
+            >
+              {diet}
+            </Typography>
+          ))}
+        </Grid2>
+
+        <Grid2 container gap="3px" sx={{ marginTop: '20px' }}>
+          {currentPizza.toppings.map((topping, index) => (
+            <Typography variant="body2" key={topping}>
+              {topping}
+              {index < currentPizza.toppings.length - 1 ? ', ' : ''}
+            </Typography>
+          ))}
+        </Grid2>
       </CardContent>
       <CardActions sx={{ display: 'flex', justifyContent: 'center', alignItems: 'flex-end' }}>
         <Button
