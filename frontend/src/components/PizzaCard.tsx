a6147ae6c98836e39ee6c8f08d12093300333e71
KrisztinaBrczan <brczan.kriszti@gmail.com>
2025-03-25 22:52:29 +0100

    replace manual diet labels with MUI's chip

--- frontend/src/components/PizzaCard.tsx
+++ frontend/src/components/PizzaCard.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { Pizza } from '@/Types';
-import { Box, Button, Card, CardActions, CardContent, CardMedia, Grid2, Typography } from '@mui/material';
+import { Box, Button, Card, CardActions, CardContent, CardMedia, Chip, Grid2, Stack, Typography } from '@mui/material';
 
 interface PizzaCardProps {
   currentPizza: Pizza;
@@ -19,23 +19,11 @@ export default function PizzaCard({ currentPizza }: PizzaCardProps) {
           {currentPizza.name}
         </Typography>
 
-        <Grid2 container spacing={2} justifyContent="center" sx={{ marginTop: '16px' }}>
+        <Stack direction="row" spacing={1} sx={{ marginTop: '16px', justifyContent: 'center' }}>
           {currentPizza.diet.map((diet) => (
-            <Typography
-              key={diet}
-              sx={{
-                border: '0.5px solid',
-                borderColor: 'secondary.main',
-                borderRadius: '25px',
-                textAlign: 'center',
-                padding: '5px',
-                fontSize: '12px',
-              }}
-            >
-              {diet}
-            </Typography>
+            <Chip label={diet} variant="outlined" color="secondary" size="small" />
           ))}
-        </Grid2>
+        </Stack>
 
         <Grid2 container gap="3px" sx={{ marginTop: '20px' }}>
           <Typography variant="body2">{currentPizza.toppings.join(', ')}</Typography>
