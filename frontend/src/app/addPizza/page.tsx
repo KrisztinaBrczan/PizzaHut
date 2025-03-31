15e6845d93a5aa9c2164cc72644d0305624ab340
KrisztinaBrczan <brczan.kriszti@gmail.com>
2025-03-31 15:58:49 +0200

    fix errors regarding useFieldArrays

--- frontend/src/app/addPizza/page.tsx
+++ frontend/src/app/addPizza/page.tsx
@@ -3,7 +3,7 @@
 import { Box, Button, IconButton, FormLabel, Grid2, Typography } from '@mui/material';
 import AddCircleIcon from '@mui/icons-material/AddCircle';
 import RemoveCircleOutlineIcon from '@mui/icons-material/RemoveCircleOutline';
-import { Controller, FormProvider, useFieldArray, useForm } from 'react-hook-form';
+import { FormProvider, useFieldArray, useForm } from 'react-hook-form';
 import { FormInputType } from '@/Types';
 import FieldArrays from '@/components/FieldArrays';
 import RadioButtons from '@/components/RadioButtons';
@@ -14,28 +14,47 @@ import { zodResolver } from '@hookform/resolvers/zod';
 const inputFields: FormInputType[] = [
   { label: 'Name', inputName: 'name', placeholderText: "pizza's name" },
   { label: 'Image', inputName: 'photo_url', placeholderText: 'url' },
-  { label: 'Price (in HUF)', inputName: 'price', placeholderText: 'HUF' },
+  { label: 'Price', inputName: 'price', placeholderText: 'HUF' },
 ];
 
-const pizzaSizeFields: number[] = [28, 32, 52];
+const pizzaSizeFields: string[] = ['28', '32', '52'];
 
 const schema = z.object({
-  name: z.string().transform((value) => value.toLowerCase().charAt(0).toUpperCase() + value.slice(1).toLowerCase()),
-  photo_url: z.string(),
-  price: z.string().transform((value: string) => Number(value)),
-  size: z.string().transform((value: string) => Number(value)),
-  diet: z.array(
-    z
-      .string()
-      .min(1, 'Provide a diet')
-      .transform((value) => value.toLowerCase().charAt(0).toUpperCase() + value.slice(1).toLowerCase()),
-  ),
-  toppings: z.array(
-    z
-      .string()
-      .min(1, 'Provide a topping')
-      .transform((value) => value.toLowerCase().charAt(0).toUpperCase() + value.slice(1).toLowerCase()),
-  ),
+  name: z
+    .string()
+    .min(1, 'Field is required')
+    .transform((value) => value.toLowerCase().charAt(0).toUpperCase() + value.slice(1).toLowerCase()),
+  photo_url: z.string().min(1, 'Field is required'),
+  price: z.string().min(1, 'Field is required'),
+  size: z.string().min(1, 'Field is required'),
+
+  diets: z
+    .array(
+      z.object({
+        value: z
+          .string()
+          .min(1, 'Provde a diet')
+          .transform((value) => value.toLowerCase().charAt(0).toUpperCase() + value.slice(1).toLocaleLowerCase()),
+      }),
+    )
+    .refine((value) => value.length > 0, {
+      message: 'Add a diet',
+      path: ['diets'],
+    }),
+
+  toppings: z
+    .array(
+      z.object({
+        value: z
+          .string()
+          .min(1, 'Provde a topping')
+          .transform((value) => value.toLowerCase().charAt(0).toUpperCase() + value.slice(1).toLocaleLowerCase()),
+      }),
+    )
+    .refine((value) => value.length > 0, {
+      message: 'Add a diet',
+      path: ['toppings'],
+    }),
 });
 
 type PizzaFormType = z.infer<typeof schema>;
@@ -45,10 +64,10 @@ export default function Page() {
     defaultValues: {
       name: '',
       photo_url: '',
-      price: 0,
-      size: 28,
-      diet: [],
-      toppings: [],
+      price: '',
+      size: '28',
+      diets: [{ value: '' }],
+      toppings: [{ value: '' }],
     },
     mode: 'all',
     resolver: zodResolver(schema),
@@ -60,7 +79,7 @@ export default function Page() {
     remove: removeDiet,
   } = useFieldArray({
     control: methods.control,
-    name: 'diet',
+    name: 'diets',
   });
 
   const {
@@ -74,6 +93,8 @@ export default function Page() {
 
   function onSubmit(data: PizzaFormType) {
     console.log(data);
+    //convert to numbers
+    //convert to strings in an array (diet and toppings)
   }
 
   return (
@@ -90,19 +111,20 @@ export default function Page() {
 
           <FormLabel id="pizza-sizes">Pizza size (cm):</FormLabel>
           {pizzaSizeFields.map((pizzaSize) => (
-            <RadioButtons key={pizzaSize} pizzaSize={pizzaSize} name="size" />
+            <RadioButtons key={pizzaSize} pizzaSize={Number(pizzaSize)} name="size" />
           ))}
 
           <Grid2 container alignItems="center" sx={{ mt: 2 }}>
             <FormLabel id="pizza-diet">Add diet:</FormLabel>
-            <IconButton onClick={() => appendDiet('')}>
+            <Typography sx={{ color: 'primary' }}>{methods.formState.errors?.diets?.message}</Typography>
+            <IconButton onClick={() => appendDiet({ value: '' })}>
               <AddCircleIcon sx={{ color: 'primary.main' }} />
             </IconButton>
           </Grid2>
 
           {dietFields.map((currentItem, index) => (
             <Box key={currentItem.id} sx={{ mt: 2, display: 'flex', alignItems: 'center' }}>
-              <FieldArrays name="diet" index={index} />
+              <FieldArrays name="diets" index={index} />
               <IconButton onClick={() => removeDiet(index)} color="error">
                 <RemoveCircleOutlineIcon />
               </IconButton>
@@ -111,7 +133,7 @@ export default function Page() {
 
           <Grid2 container alignItems="center" sx={{ mt: 2 }}>
             <FormLabel id="pizza-toppings">Add toppings:</FormLabel>
-            <IconButton onClick={() => appendTopping('')}>
+            <IconButton onClick={() => appendTopping({ value: '' })}>
               <AddCircleIcon sx={{ color: 'primary.main' }} />
             </IconButton>
           </Grid2>
--- frontend/src/components/FieldArrays.tsx
+++ frontend/src/components/FieldArrays.tsx
@@ -2,19 +2,28 @@ import { Box, IconButton, TextField } from '@mui/material';
 import { Controller, useFormContext } from 'react-hook-form';
 
 interface FieldArraysProps {
-  name: string;
+  name: 'diets' | 'toppings';
   index: number;
 }
 
 export default function FieldArrays({ name, index }: FieldArraysProps) {
-  const { control } = useFormContext();
+  const { control, formState } = useFormContext();
 
   return (
     <>
       <Controller
         control={control}
-        name={`${name}[${index}]`}
-        render={({ field }) => <TextField {...field} size="small" placeholder={`${name} ${index + 1}`} />}
+        name={`${name}[${index}].value`}
+        render={({ field }) => (
+          <TextField
+            {...field}
+            defaultValue={field.value}
+            size="small"
+            placeholder={`${name} ${index + 1}`}
+            error={!!formState.errors[name]}
+            helperText={"Don't leave it empty"}
+          />
+        )}
       />
     </>
   );
